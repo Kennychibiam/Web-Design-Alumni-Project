3f766eceedcdbecc0e9c83e38f2bf41d7b902191
Agha-Ibiam Chijioke Kenny <kenchibiam@gmail.com>
2021-04-01 23:51:22 +0100

    Update login.php

--- login.php
+++ login.php
@@ -1,86 +1,14 @@
+<?php include "head.php"?>
+
+
 <!DOCTYPE html>
-
 <html>
-
 <head>
-<title>Login</title>
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Log In</title>
 
 <style>
-
-body{
-background-color: #ffffff;
-
-}
-
-.header{
-background-color:#ffffff;
-padding:10px;
-
-position:fixed;
-top:0px;
-left:0px;
-right:0px;
-z-index:30px;
-}
-#dashdropdown  {
-  position:absolute;
-  z-index: 2;
-  min-width:170px;
-  height:auto;
-  background-color:#FFFFFF;opacity:0.9;
-  display:none;
-  padding-top:7px;
-}
-
-.usericons{
-   top: 50%;
-  -ms-transform: translateY(-50%);
-  transform: translateY(-50%);
-  display:inline-block;
-  position:absolute;
-  right:45px;
-}
-div#dashdropdown a{
-text-decoration:none;
-color:black;
-}
-
-#tooltip{
-  position:absolute;
-  border-radius: 6px;
-  background-color:#FFFFFF;opacity:0.9;
-  min-width:80px;
-  margin-top:5px;
-    display:none;
-
-}
-#tooltext{
-  text-align:center;
-  font-size:18px;
-    display:none;
-
-}
-#cartlink{
-	text-decoration:none;
-	color:black;
-	
-}
-#myaccount{
-padding-left:8px;
-padding-right:8px;
-margin:auto;
-}
-
-
-
-.header a[href="#index"]{
-text-decoration:none;
-color:black;
-}
-
-
-
+input:-webkit-autofill {
+-webkit-box-shadow: 0 0 0 1000px white inset !important;}
 
 #loginform{
 	position:absolute;
@@ -91,35 +19,17 @@ color:black;
     transform: translate(-50%, -50%);
 	
 }
-
-#login{
-padding-left:8px;
-padding-right:8px;
-margin:auto;
-}
-
-#spancreateaccount{
-	text-align:center;
-	
-}
 form input{
 	border-left:none;
     border-right:none;
 	border-top:none;
 	background-color:#ffffff;
-
+    width:100%;
 	height:50px;
 	
-}
-form input:focus{
-	
-	outline:none;
-    box-shadow: 0 0 10px #719ECE;
-	
+} 
 
-	
-}
-#submitbutton{
+#submit,.reset{
 	margin-top:30px;
 	display:block;
 	width:100%;
@@ -132,183 +42,148 @@ form input:focus{
     cursor: pointer;
 	
 }
-#submitbutton:hover{
-		background-color: #4CAF50; 
+
+form input:focus{
+	
+	outline:none;
+    box-shadow: 0 0 10px #719ECE;
+	
 
 	
 }
+  #submit:hover{
+		background-color: #4CAF50; 
+}
+ .reset:hover{
+		background-color: #4CAF50; 
+}
+
+#divpassword{
+	margin-top:20px;
+	
+	
+}
+a[href="createaccount.php"]{
+	color:#EE7500;
+	text-decoration:none;
+	
+}
+.hidden{display:none;}
 
 </style>
+
 
 </head>
 
 <body>
-<div class="header">
-<a href="#index" >
-<span style="font-size:35px;font-family:Verdana,sans-serif;">
-<img src="images/alumnicon.png" width="50px" height="50px" style="vertical-align:middle"/>AlumniDonate</span>
-</a>
-
-
-
-<div class="usericons">
-
-
-
-<div id="dashboardinfo" style="margin-right:20px; display:inline-block; position:relative" onmouseover="userDashDropDownHover()" onmouseout="userDashDropDownHoverOut()">
-
-<a href="">
-
-<img src="images/user.png" width="50px" height="35px" />
-</a>
-<div id="dashdropdown">
-<div id="myaccount" style="padding-bottom:13px;" onmouseover="myAccountHover()" onmouseout="myAccountHoverOut()">
-
-<a href="">
-<span id="myaccountspan" > <img id="myaccountimage" src="images/dashboard.png" width="20px" height="20px" style="vertical-align:middle;margin-right:10px"/>My Account</span>
-</a>
-</div>
-<a href="login.php" target="_blank">
-<div style="padding-bottom:13px;" id="login" onmouseover="loginHover()" onmouseout="loginHoverOut()" >
-
-<span  id="loginspan"><img id="loginimage" src="images/login.png" width="20px" height="20px" style="vertical-align:middle;margin-right:10px"/>Login/Sign Up</span>
-</div>
-</a>
-</div>
-
-</div>
-
-<a id="cartlink" href="">
-<div id="cart" style="display:inline-block; position:relative" >
-
-
-<img src="images/cart.png" id="cartimage" width="50px" height="35px" onmouseover="cartHover()" onmouseout="cartHoverOut()"/>
-
-<div id="tooltip" >
-<span id="tooltext">View Cart</span>
-</div>
-​</div>
-</a>
-</div>
-
-</div>
 
 
 
 <div id="loginform">
-<div  id="spancreateaccount"><span   style="font-size:30px;color:grey;">CREATE AN ACCOUNT</span></div>
+<form id="form" method="POST" action="login.php" autocomplete="off">
 
-<form   action="login.php" method="POST" autocomplete="on" >
-<div style="display:inline-block">  <input type="text" placeholder="First Name" name="firstname" style="width:300px" required/>  </div>
-<div style="display:inline-block;margin-left:20px" >  <input type="text" placeholder="Last Name" name="lastname"style="width:300px" required/>  </div>
+<div><input type="text" id="matricno" name="matricno" placeholder="Matric No" autocomplete="off" required></div>
 
-<div class="matricpassword" style="margin-top:50px">
+<div id="divpassword"><input type="password"  id="password" name="password"   placeholder="Password"   required></div>
 
-<div style="display:inline-block;"><input type="email" name="email" placeholder="Email" style="width:300px" required></div>
 
-<div  style="display:inline-block;margin-left:20px" ><input type="text" name="matricno" placeholder="Matric No" style="width:300px" required></div>
+
+
+<div>
+<input type="submit" id="submit" value="LOG IN"  autocomplete="off">
 </div>
-
-<div style="margin-top:50px;" >
-<div style="display:inline-block"><span style="margin-right:10px;">(+234)</span><input type="tel" placeholder="Phone" name="phone" size="11" style="width:230px" required></div>
-
-<div  style="display:inline-block;margin-left:20px">
-
-<div><span style="color:grey">You can change this later</span></div>
-<label style="position:relative;color:grey" for="alumni">Alumni</label> <input style="position:relative;top:20px;bottom:0px" id="alumni"type="radio" name="selectstatus" value="alumni"required>
-
-<label style="margin-left:20px;color:grey" for="undergrad">Undergraduate</label> <input style="margin-left:20px;position:relative;top:20px" id="undergrad" type="radio" name="selectstatus" value="undergraduate" required > </div>
-
+<div><input type="reset" class="reset" value="RESET">
 </div>
-
-
-<div style="display:inline-block" ><input style="margin-top:50px;width:300px" type="password" name="password" placeholder="Password" value="" required></div>
-<div style="display:inline-block"  ><input style="margin-top:50px;width:300px;margin-left:20px"type="password" name="confirm" placeholder="Confirm "  required></div>
-
-<input type="button" id="submitbutton" value="CREATE ACCOUNT">
-
-
-
-
-
-
-
 </form>
 
+
+<div style="margin-top:10px">Don't have an account.<a href="createaccount.php" <span>Create Account</span></a></div>
 </div>
 
-<script>
 
-function userDashDropDownHover(){
-document.getElementById("dashdropdown").style.display="block";
-}
-function userDashDropDownHoverOut(){
-document.getElementById("dashdropdown").style.display="none";
-}
-function cartHover(){
-document.getElementById("tooltip").style.display="block";
-document.getElementById("tooltext").style.display="block";
-document.getElementById("cartimage").src="images/cartorange.png";
-
-
-}
-function myAccountHover(){
-document.getElementById("myaccount").style.backgroundColor="#FF7500";
-document.getElementById("myaccountspan").style.color="white";
-document.getElementById("myaccountimage").src="images/dashboardwhite.png";
-
-
-
-}
-function loginHover(){
-document.getElementById("login").style.backgroundColor="#FF7500";
-document.getElementById("loginspan").style.color="white";
-document.getElementById("loginimage").src="images/loginwhite.png";
-}
-
-function loginHoverOut(){
-document.getElementById("login").style.backgroundColor="white";
-document.getElementById("loginspan").style.color="black";
-document.getElementById("loginimage").src="images/login.png";
-}
-
-function myAccountHoverOut(){
-document.getElementById("myaccount").style.backgroundColor="white";
-document.getElementById("myaccountspan").style.color="black";
-document.getElementById("myaccountimage").src="images/dashboard.png";
-
-}
-
-function cartHoverOut(){
-document.getElementById("tooltip").style.display="none";
-document.getElementById("tooltext").style.display="none";
-document.getElementById("cartimage").src="images/cart.png";
-
-}
-</script>
 
 
 </body>
 
-</html>
-</php
-fuction processLogIn(){
-	$formNames=["firstname","lastname","email","matricno","phone","password","confirm"];
-	$canProcess=true;
-	
-	foreach($formNames as $field){
-		if(!isset($_POST[$field])){
-			$canProcess=false;
-		}
-		
-	
-	if(canProcess){
-		
-		
-	}
-}
 
+</html>
+
+
+<?php 
+ 
+ 
+ if(isset($_POST["matricno"])&&isset($_POST["password"])){
+ $host = "localhost";
+$username = "root";
+$databasepassword = "database1234567";
+$databasename= "alumni_donate";
+
+$matricno= $_POST["matricno"];
+$password= $_POST["password"];
+
+
+ $conn=mysqli_connect($host,$username,$databasepassword,$databasename);
+ if(!$conn){
+	 
+	 	die("Connection failed: " . mysqli_connect_error());
+
+	 
+ }
+ else{
+	$sql="SELECT * FROM user WHERE matricno= matricno AND password='$password'"; 
+    $result=mysqli_query($conn,$sql);
+	
+	if(mysqli_num_rows($result)>0){
+		 $_SESSION["isLoggedIn"]=true;
+		 $checkIfUserNameExists=0;
+		 
+		while($row=mysqli_fetch_assoc($result)){
+			if($row["username"]==null){
+              $checkIfUserExists=null;
+			$_SESSION["username"]=$row["firstname"];
+			}else{
+				$_SESSION["username"]=$row["username"];
+                $checkIfUserNameExists=$row["username"];
+
+			}
+			
+			$_SESSION["status"]=$row["status"];
+			
+		}         
+		    if(is_null($checkIfUserNameExists)){
+				echo "<script>alert('Username doesn't Exists')</script>";
+
+			}
+			else{
+				echo "<script>alert('Username Exists')</script>";
+			}
+			
+			
+			
+
+	}else{
+				 $_SESSION["isLoggedIn"]=false;
+
+				echo "<script>alert('Account does not exist')</script>";
+
+
+	}
+
+ }
+ 
+ }
+
+	 				 $_POST=array();
 
 ?>
-​
 
+<script>
+
+var matric=document.getElementById("matricno");
+matricno.value="";
+
+var password=document.getElementById("password");
+password.value="";
+
+document.getElementById("form").reset();
+</script>
